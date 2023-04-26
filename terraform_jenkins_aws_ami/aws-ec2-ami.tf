47ec3b565eaf2b901cfbe2dbde6596e08514df2b
hermann90 <hermannchefouet@gmail.com>
2023-04-26 20:37:23 +0200

    update ami

--- terraform_jenkins_aws_ami/aws-ec2-ami.tf
+++ terraform_jenkins_aws_ami/aws-ec2-ami.tf
@@ -12,6 +12,18 @@ resource "aws_instance" "jenkins_ec2_instance" {
   vpc_security_group_ids = [aws_security_group.jenkins_security_gp.id]
   key_name               = aws_key_pair.instance_key.key_name
 
+ # Set the instance's root volume to 40 GB
+  root_block_device {
+    volume_size = 30
+  }
+
+  # Attach an additional 40 GB EBS volume
+  ebs_block_device {
+    device_name = "/dev/xvdf"
+    volume_size = 30
+    volume_type = "gp2"
+  }
+
   tags = {
     Name = "jenkins-server"
     Owner = "Hermann90"
