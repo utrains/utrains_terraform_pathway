b7f8b8195b463e09da1f59dfa2533c5c62ac698e
kserge2001 <kserge2001@yahoo.fr>
2023-04-29 09:21:33 -0500

    Update output.tf

--- terraform_jenkins_aws_ami/output.tf
+++ terraform_jenkins_aws_ami/output.tf
@@ -7,11 +7,3 @@ output "jenkins_url" {
   value     = join ("", ["http://", aws_instance.jenkins_ec2_instance.public_dns, ":", "8080"])
 }
 
-# print the url of the nexus server
-output "nexus_ssh_connection_command" {
-  value     = join ("", ["ssh -i instance_key_pair.pem ec2-user@", aws_instance.nexus_ec2_instance.public_dns])
-}
-# print the url of the jenkins server
-output "nexus_url" {
-  value     = join ("", ["http://", aws_instance.nexus_ec2_instance.public_dns, ":", "8081"])
-}
