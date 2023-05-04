a5b9eb4dc08906183dac66529765c84b9deaf16c
kserge2001 <kserge2001@yahoo.fr>
2023-05-04 13:42:42 -0500

    Update security_gp.tf

--- terraform_jenkins_aws_ami/security_gp.tf
+++ terraform_jenkins_aws_ami/security_gp.tf
@@ -34,34 +34,3 @@ resource "aws_security_group" "jenkins_security_gp" {
   }
 }
 
-# create security group for the nexus instance
-resource "aws_security_group" "nexus_security_gp" {
-  name        = "nexus security group"
-  description = "allow access on ports 8081 and 22 for nexus and ssh"
-  vpc_id      = aws_default_vpc.default_vpc.id
-  # allow access on port 8081 for nexus Server
-  ingress {
-    description      = "httpd access port"
-    from_port        = 8081
-    to_port          = 8081
-    protocol         = "tcp"
-    cidr_blocks      = ["0.0.0.0/0"]
-  }
-  # allow access on port 22 ssh connection
-  ingress {
-    description      = "ssh access"
-    from_port        = 22
-    to_port          = 22
-    protocol         = "tcp"
-    cidr_blocks      = ["0.0.0.0/0"]
-  }
-  egress {
-    from_port        = 0
-    to_port          = 0
-    protocol         = -1
-    cidr_blocks      = ["0.0.0.0/0"]
-  }
-  tags   = {
-    Name = "nexus security group"
-  }
-}
